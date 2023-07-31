19c5ccc56b4118010a65b61b1be4f986d84ae7be
sandeepjampula <jampulasandeep@gmail.com>
2023-07-31 06:44:34 +0530

    maven project add

---
+++ src/main/java/com/tcs/maven_demo_project/App.java
@@ -0,0 +1,13 @@
+package com.tcs.maven_demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this maven project" );
+    }
+}
